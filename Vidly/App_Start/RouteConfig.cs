e10d6be725d967e59fbd7f111fc400a4c2398376
nabsabt <nabsabt@gmail.com>
2020-04-05 18:38:31 +0200

    added custom routeconfig

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,6 +12,18 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //now we create a custom route. We need to add it before the default route, because the order of them matters. They needed to be defined from most
+            //specific to most generic
+            routes.MapRoute(
+                "MoviesByReleaseDate",  //name of the route
+                "movies/released/{year}/{month}", //url pattern of the route
+                new {controller = "Movies", action = "ByReleaseDate"},
+                //default: we set an anonym object with the name of controller and action name. 
+                //We set the ByReleaseDate action in the controller (MoviesController) class.
+                new { year= @"\d{4}", month=@"\d{2}"}  //here we make it look nicer, by setting year to 4 digits, and month to 2 digits in an anonym object
+                                                       //This will force the user to add only 4 digits to year, and 2 for month in the url bar. If he types /2015/1, it will throw a 404 error
+                );                                                     
+
 
             routes.MapRoute(
                 name: "Default",
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -17,12 +17,49 @@ namespace Vidly.Controllers
         {
             Movie movie = new Movie() {Name = "Shawhank Redemption"};
             //here I created a Movie object, called "movie". I gave property "Name" a value: "Shawhank Redemption"
-
+            
 
             return View(movie);
             //I added the movie object (Model) to the View as parameter. This will render the movie Model, by the rules, seen in the Random.cshtml file (View)
             //ViewResult (View) is one of the ActionResults, that is why this method can return it, though in the initialization, its type is ActionResult
             // ViewResult is an ActionResult type, View() is the helper method of it
         }
+
+        public ActionResult Edit (int id)
+        {
+            return Content("id=" + id);
+            //here I create an action. The parameter "id" takes a value, and the Content() method prints it out.
+            //I can check it by typing /movies/edit/1, where the movies is the controller(?), the /edit calls the action result, and the /1 is the
+            //optional parameter, we pass. It can be whatever we want in this case
+
+            //// the parameter must be named "id" in this case, as in RouteConfig line 18 we call it {id}:
+            //url: "{controller}/{action}/{id}"
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            //we marked pageIndex nullable (optional) with sign "?". sortBy is a reference type (string), so its nullable by default
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            //if pageIndex paramater is not defined, we start with the index 1, if sortBy is not defined, we sort by Name
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+
+            return Content(year+"/"+month);
+            //the Content action here will print the year+month, given by us (user) in the navigation bar ( /movies/released/2015/11 for ex, 
+            //will be printed like: 2015/11 in the body
+            
+        }
+        
     }
 }
